8a44def8dd0383325ca818f7d4ad6cdbb71429c6
Evgeniy <zadrod93@yandex.ru>
2022-05-12 21:54:15 +0300

    Задача 35

---
+++ Task_35.py
@@ -0,0 +1,18 @@
+# 35.	В файле находится N натуральных чисел, записанных через пробел. 
+# Среди чисел не хватает одного, чтобы выполнялось условие A[i] - 1 = A[i-1]. 
+# Найдите это число.
+
+file = open('Numbers.txt', 'w')
+file.write('0 1 2 4 5 6 7 8 9 10 11 12')
+
+file = open('Numbers.txt', 'r')
+strnumbers = file.readline()
+print(strnumbers)
+
+splitnum = strnumbers.split()
+for i in range(0, len(splitnum)):
+    if int(splitnum[i]) + 1 != i + 1:
+        findum = i
+        break
+print(findum)
+file.close
